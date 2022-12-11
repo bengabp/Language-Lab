998ed3005292f47b8cb91a9e4141c32db4186034
Onyebuchi Benedict <84660669+Bengab-coder@users.noreply.github.com>
2022-12-11 21:32:01 +0100

    Implemented authentication app

--- authentication_app/urls.py
+++ authentication_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import auth_login,auth_register,auth_logout
+from .views import auth_login,auth_register,auth_logout,auth_reset_password
 
 urlpatterns = [
     path('login',auth_login,name='login'),
     path('register',auth_register,name='register'),
-    path('logout',auth_logout,name='logout')
+    path('logout',auth_logout,name='logout'),
+    path("reset-password",auth_reset_password,name="reset_password")
 ]
--- authentication_app/views.py
+++ authentication_app/views.py
@@ -1,18 +1,48 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.hashers import make_password,check_password
+
+from chat_app.models import  UserAccount,Language
 
 # Create your views here.
 def auth_login(request):
-    print(request.POST)
-    return HttpResponse("Login")
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = UserAccount.objects.filter(username=username).first()
+        if user:
+            if check_password(password,user.password):
+                login(request,user)
+                return HttpResponseRedirect("/")
+            else:
+                return HttpResponse("incorrect password")
+        else:
+            return HttpResponse("no records found for username")
+
+    return render(request,"signin.html")
 
 def auth_register(request):
-    print(request.POST)
-    return HttpResponse("Register")
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        user = UserAccount.objects.filter(username=username).first()
+        if user:
+            return HttpResponse("username not available")
+        else:
+            user = UserAccount(username=username,email=email,password=make_password(password))
+            user.save()
+            login(request,user)
+            return HttpResponseRedirect("/")
+            
+    return render(request,"signup.html")
 
 def auth_logout(request):
-    print(request.POST)
-    return HttpResponse("Logout")
+    logout(request)
+    return HttpResponseRedirect("/")
+
+def auth_reset_password(request):
+    return HttpResponse("resetting password")
 
